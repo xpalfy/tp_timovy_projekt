4aad7df603ad16ffca03c4f46605c6f079df0df9
xpalfy <xpalfy@stuba.sk>
2024-10-22 19:13:51 +0200

    Adding sql or users table

---
+++ sql/users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL UNIQUE,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL
+);
