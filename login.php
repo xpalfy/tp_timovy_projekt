9157b4153fe3bc1ea9b88d610ed4d27e195f1dcf
xpalfy <xpalfy@stuba.sk>
2024-10-29 20:17:46 +0100

    Add pictures.sql for saving picture's path

--- login.php
+++ login.php
@@ -96,12 +96,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form action="login.php" method="POST">
             <div class="form-group">
                 <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" name="username" oninput="isValidInput(this)"
+                <input type="text" class="form-control" id="username" name="username"
                        required>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password" oninput="isValidInput(this)"
+                <input type="password" class="form-control" id="password" name="password"
                        autocomplete="off" required>
             </div>
             <button type="submit" class="btn btn-primary">Log In</button>
