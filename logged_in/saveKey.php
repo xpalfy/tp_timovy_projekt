a14dffb31896ae52ededaf2c21ec63486b24ee6e
xnyitrai <xnyitrai@stuba.sk>
2024-11-20 18:09:53 +0100

    TEST

--- logged_in/saveKey.php
+++ logged_in/saveKey.php
@@ -17,21 +17,38 @@ require_once '../config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $post = json_decode(file_get_contents('php://input'), true);
 
-    if (empty($post['data']) || empty($post['data_name'])) {
+    if (empty($post['data']) || empty($post['data_name']) || empty($post['user_name']) || empty($post['id'])) {
         http_response_code(400);
         echo json_encode(['error' => 'Invalid input data']);
         exit;
     }
 
+    if ($post['user_name'] !== $_SESSION['user']['username'] || $post['id'] !== $_SESSION['user']['id']) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid user']);
+        exit;
+    }
+
+    if (!preg_match('/^[a-zA-Z0-9_\-]+$/', $post['data_name'])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid file name']);
+        exit;
+    }
+
     $data = $post['data'];
+    $data_name = $post['data_name'];
+    $user_name = $post['user_name'];
+    $id = $post['id'];
+
     if (strpos($data, 'data:image/png;base64,') === 0) {
-        $data = explode(',', $data)[1]; // Strip the base64 header
+        $data = explode(',', $data)[1]; 
     }
     $data = base64_decode($data);
 
-    $directory = realpath(__DIR__ . '/../KEYS/');
-    if (!is_dir($directory)) {
-        if(mkdir($directory, 0777, true)){
+    $directory_path = realpath(__DIR__ . '/..') . '/KEYS/' . $user_name;
+    echo $directory_path;
+    if (!is_dir($directory_path)) {
+        if(mkdir($directory_path, 0777, true)){
             echo 'Directory created';
         } else {
             http_response_code(400);
@@ -40,16 +57,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 
-    $file = $post['data_name'] . '.png';
-    echo ini_get('open_basedir');
+    $file_path = $directory_path . '/' . $data_name . '.png';
 
+    // check if picture is in DB
+    $conn = getDatabaseConnection();
+    $stmt = $conn->prepare('SELECT * FROM pictures WHERE creator = ? AND path = ?');
+    $stmt->bind_param('is', $id, $file_path);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        http_response_code(400);
+        echo json_encode(['error' => 'File already exists']);
+        $stmt->close();
+        $conn->close();
+        exit;
+    }
+
+    $stmt = $conn->prepare('INSERT INTO pictures (creator, path) VALUES (?, ?)');
+    $stmt->bind_param('is', $id, $file_path);
+    $stmt->execute();
     
-    if (file_put_contents($directory .'/'. $file, $data) !== false) {
-        echo json_encode(['success' => 'File saved', 'filename' => $file]);
+    
+    if (file_put_contents($file_path, $data) !== false) {
+        echo json_encode(['success' => 'File saved', 'filename' => $data_name]);
     } else {
         http_response_code(400);
         echo json_encode(['error' => 'Failed to save file']);
+        // delete from DB
+        $stmt = $conn->prepare('DELETE FROM pictures WHERE creator = ? AND path = ?');
+        $stmt->bind_param('is', $id, $file_path);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
         exit;
     }
+
+    $stmt->close();
+    $conn->close();
 }
 ?>
