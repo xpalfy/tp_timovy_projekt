6182a041b1c5c838dc0a6cf39b243de6874e5bda
xpalfy <xpalfy@stuba.sk>
2025-04-26 14:27:09 +0200

    change pictures on the pages

--- login.php
+++ login.php
@@ -124,7 +124,7 @@
 
   <!-- Login Section -->
   <main class="flex-grow flex items-center justify-center px-4">
-    <div class="glass p-10 w-full max-w-md my-10" data-aos="fade-up">
+    <div class="glass p-10 w-full max-w-md my-10" data-aos="fade-down">
       <h2 class="text-3xl font-bold text-center mb-6">Login to HandScript</h2>
       <form id="loginForm" class="space-y-4">
         <div>
