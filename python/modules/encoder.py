d7c62088fd2363ffe903088e477a8f2cbff4cb1b
BenceBoth115378 <bothbence62@gmail.com>
2025-05-05 21:16:21 +0200

    feat: enhance segmentation functionality and improve error handling in segmentModule and segmentator

--- python/modules/encoder.py
+++ python/modules/encoder.py
@@ -2,7 +2,7 @@ import json
 import os
 class Encoder:
     @staticmethod
-    def get_json():
+    def get_key_json():
         # Define the path to the JSON file
         file_path = os.path.join(os.path.dirname(__file__), "example_json.json")
         
@@ -11,4 +11,22 @@ class Encoder:
             data = json.load(file)
         
         return data
+    
+    def get_cipher_json(self):
+        # Define the path to the JSON file
+        file_path = os.path.join(os.path.dirname(__file__), "example_json.json")
+        
+        # Open and read the JSON file
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        
+        return data
+    
+    def encode_keys(self, polygons, ):
+        for polygon in polygons:
+            coordinates = polygon['coordinates']
+            polygon_type = polygon['type']
+            #TODO: Add encoding logic here
+        
+        return self.get_key_json()
         
