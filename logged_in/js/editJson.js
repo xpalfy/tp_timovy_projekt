6d18d08e75bdfaa7f2c8015754bc2bb6d3c996a5
xnyitrai <xnyitrai@stuba.sk>
2025-05-13 15:42:44 +0200

    refactor: editJsonModule.php into js files

---
+++ logged_in/js/editJson.js
@@ -0,0 +1,71 @@
+import { version } from './version.js';
+
+Promise.all([
+    import(`./ui-animation-handler.js${version}`),
+    import(`./modules-helper.js${version}`)
+]).then(([uiAnimationHandlers, modulesHelper]) => {
+    const {
+        goToAnalyzation, fetchItems, saveJson,
+        fetchDocuments, showJsonEditor, fetchJson, downloadJSON,
+        getItemsData, setSelectedItemImagePath,
+        getDocumentsData, setSelectedDocumentId, getSelectedDocumentId, setSelectedItemId,
+        getSelectedItemId
+    } = modulesHelper;
+    const {
+        setupNavbarToggle, setupGSAPAnimations, setStep, checkToasts, hideLoading, showLoading
+    } = uiAnimationHandlers;
+
+    console.log("Modules loaded successfully");
+
+
+    // Expose functions to the global scope
+    window.goToAnalyzation = goToAnalyzation; // Expose goToAnalyzation
+    window.downloadJSON = downloadJSON; // Expose downloadJSON
+    window.saveJson = saveJson; // Expose saveJson
+    window.showLoading = showLoading; // Expose showLoading
+
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        initializePage();
+    } else {
+        document.addEventListener('DOMContentLoaded', initializePage);
+    }
+
+    function initializePage() {
+        fetchDocuments('PROCESSED');
+        $("#documentSearch").autocomplete({
+            source: function (request, response) {
+                const term = request.term.toLowerCase();
+                const filtered = getDocumentsData().filter(doc => doc.title.toLowerCase().includes(term));
+                response(filtered.map(doc => ({
+                    label: doc.title + ' (' + doc.doc_type.toLowerCase() + ')',
+                    value: doc.title,
+                    id: doc.id
+                })));
+            },
+            select: function (event, ui) {
+                setSelectedDocumentId(ui.item.id);
+                $("#itemSelector").prop("disabled", false);
+                $("#itemSelector").empty();
+                $("#itemSelector").append('<option value="" disabled selected>Select an item</option>');
+                fetchItems(getSelectedDocumentId(), null, 'PROCESSED');
+            },
+            minLength: 1
+        });
+
+        $("#itemSelector").change(function () {
+            showLoading();
+            setSelectedItemId($(this).val());
+            setSelectedItemImagePath(getItemsData().find(item => item.id == getSelectedItemId()).image_path);
+            showJsonEditor();
+            fetchJson();
+            hideLoading();
+        });
+        setupNavbarToggle();
+        setupGSAPAnimations();
+        hideLoading();
+        setStep(4);
+        checkToasts();
+    }
+}).catch(error => {
+    console.error("Error loading modules:", error);
+});
