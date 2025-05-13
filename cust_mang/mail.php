0283bccc7ccfb1010a59d31d072844a8df2d42a8
xpalfy <xpalfy@stuba.sk>
2025-05-13 20:17:32 +0200

    feat: implement sending email for new question in faq

--- cust_mang/mail.php
+++ cust_mang/mail.php
@@ -6,25 +6,31 @@ require_once '../vendor/autoload.php';
 
 function sendVerificationEmail($toEmail, $code) {
     $mail = new PHPMailer(true);
+
     try {
+        // Server settings
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'ptimovy@gmail.com';
-        $mail->Password = 'cfxc llnb lspi sevg';
-        $mail->SMTPSecure = 'tls';
+        $mail->Username = 'ptimovy@gmail.com';   
+        $mail->Password = 'cfxc llnb lspi sevg';      
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
         $mail->Port = 587;
 
+        // Recipients
         $mail->setFrom('ptimovy@gmail.com', 'HandScript');
         $mail->addAddress($toEmail);
+
+        // Content
         $mail->isHTML(false);
         $mail->Subject = 'Email Verification';
         $mail->Body    = "Your verification code is: $code";
 
         $mail->send();
         return true;
+
     } catch (Exception $e) {
-        error_log("Mailer Error: " . $mail->ErrorInfo);
+        error_log("PHPMailer Error: " . $mail->ErrorInfo);
         return false;
     }
 }
